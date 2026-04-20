a94925ecc6872c1bbcecebb836b702974192a40b
ivan innocent <ivaninnocent500@gmail.com>
2026-04-20 16:31:15 +0300

    fix: real fixed

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://dubaivipodozi.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+  ]
+}
